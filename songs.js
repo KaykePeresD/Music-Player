fe1805108ae9a8ead21fbc553eb44938b160fc69
Kayke Peres <140190363+KaykePeresD@users.noreply.github.com>
2024-05-30 00:33:19 -0300

    Add files via upload

---
+++ songs.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    src: "./src/spotifydown.com - iSpy (feat. Kodak Black).mp3",
+    name: "iSpy (feat. Kodak Black)",
+    img: "./img/ispy.jpg"
+  },
+  {
+    src: "./src/spotifydown.com - It Was A Good Day.mp3",
+    name: "It Was A Good Day",
+    img: "./img/goodday.jpg"
+  },
+  {
+    src: "./src/spotifydown.com - Wasted (feat. Lil Uzi Vert).mp3",
+    name: "Wasted (feat. Lil Uzi Vert)",
+    img: "./img/wasted.jpg"
+  },
+  {
+    src: "./src/spotifydown.com - Sunflower - Spider-Man_ Into the Spider-Verse.mp3",
+    name: "Sunflower-Spider-Man",
+    img: "./img/sunflower.jpg"
+  },
+];
+
